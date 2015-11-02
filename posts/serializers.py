5b42f4024b1b7c2c0ff3c62902283035fde7d379
Vitaliy Franchook <vfranchook@gmail.com>
2015-11-02 19:17:17 +0200

    Id field isn't required in PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField()
+    id = serializers.ReadOnlyField(required=False)
     class Meta:
         model = Post
