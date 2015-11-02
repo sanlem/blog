d680f75d64cc14230344dcac1314d2e9d45c52d8
Vitaliy Franchook <vfranchook@gmail.com>
2015-11-02 18:34:57 +0200

    Configuration for Heroku

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = '2&&q77jc3t+qw9_p=lufs&4t%dxn#3%!$k&a=))cof0e)e)eur'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
