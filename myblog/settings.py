9aa69dc12b30f7dc52fccf3957a2ff88d9d658bb
Vitaliy Franchook <vfranchook@gmail.com>
2015-11-08 13:54:19 +0200

    forgot to change settings for development

--- myblog/settings.py
+++ myblog/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
 }
 
 
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
